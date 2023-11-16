c1a74b8b2290cc18837e3da85604795ea4a47bbc
Amaan117 <118039011+Amaan117@users.noreply.github.com>
2023-11-16 23:24:22 +0530

    Add files via upload

---
+++ src/Login.js
@@ -0,0 +1,86 @@
+import React from 'react'
+
+export default function Login() {
+  return (
+    <>
+    <section className="vh-80" style={{ backgroundColor: "rgb(245, 212, 223)" }}>
+  <div className="container py-5 h-100">
+    <div className="row d-flex justify-content-center align-items-center h-100">
+      <div className="col col-xl-8">
+        <div className="card" style={{ borderRadius: "1rem" }}>
+          <div className="row g-0">
+            <div className="col-md-6 col-lg-5 d-none d-md-block">
+              <img
+                src="https://img.freepik.com/premium-vector/online-education-learning-concept-with-students_662093-225.jpg?size=338&ext=jpg&ga=GA1.1.1413502914.1696550400&semt=ais"
+                alt="login form"
+                className="img-fluid"
+                style={{ borderRadius: "1rem 0 0 1rem" , height: "600px"}}
+              />
+            </div>
+            <div className="col-md-6 col-lg-7 d-flex align-items-center">
+              <div className="card-body p-4 p-lg-5 text-black">
+                <form>
+                  <div className="d-flex align-items-center mb-3 pb-1">
+                    <i
+                      className="fas fa-cubes fa-2x me-3"
+                      style={{ color: "#ff6219" }}
+                    />
+                    <span className="h1 fw-bold mb-0">FreeLancers</span>
+                  </div>
+                  <h5
+                    className="fw-normal mb-3 pb-3"
+                    style={{ letterSpacing: 1 }}
+                  >
+                    Sign into your account
+                  </h5>
+                  <div className="form-outline mb-4">
+                    <input
+                      type="email"
+                      id="form2Example17"
+                      className="form-control form-control-lg"
+                    />
+                    <label className="form-label" htmlFor="form2Example17">
+                      Email address
+                    </label>
+                  </div>
+                  <div className="form-outline mb-4">
+                    <input
+                      type="password"
+                      id="form2Example27"
+                      className="form-control form-control-lg"
+                    />
+                    <label className="form-label" htmlFor="form2Example27">
+                      Password
+                    </label>
+                  </div>
+                  <div className="pt-1 mb-4">
+                    <button
+                      className="btn btn-dark btn-lg btn-block"
+                      type="button"
+                    >
+                      Login
+                    </button>
+                  </div>
+                  
+                  <p className="mb-5 pb-lg-2" style={{ color: "#393f81" }}>
+                    Don't have an account?{" "}
+                    <a href="#!" style={{ color: "#393f81" }}>
+                      Register here
+                    </a>
+                  </p>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+    
+    
+    
+    </>
+  )
+}
